cfd97b1a095f0ae809af54fd1ae7a3ff0b06789a
Vahid SJ <vahid.vsh.68@gmail.com>
2024-02-18 08:18:39 +0100

    update my picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () { elementToggleFunc(any); });
 
 
 // contact form variables
